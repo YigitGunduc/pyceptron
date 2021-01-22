4d94b8df7679c4251f8109380b9d3b8b5cee54c3
Yigit <yy347112@gmail.com>
2021-01-22 23:36:04 +0300

    files reordered

--- loss.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-from base import Loss
-
-class MSE(Loss):
-    def loss(self, predicted, actual):
-        return np.sum((predicted - actual)**2)
-
-    def grad(self, predicted, actual):
-        return 2 * (predicted - actual)
